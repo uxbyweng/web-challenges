6a69581e140d187e99e4010a5a0a884a915a7a6b
Karsten Weng <karsten@weng.eu>
2025-12-12 11:26:13 +0100

    fix(error): correct console statement, and make eventListener classList css adding and removing correctly

--- javascript/js-dom-and-events/survey-error-fixing/js/index.js
+++ javascript/js-dom-and-events/survey-error-fixing/js/index.js
@@ -10,7 +10,7 @@ nextButton.addEventListener("click", () => {
 
 // 2. When clicking the "previous" button, this event listener is not executed. Do you know what's wrong with this code?
 const prevButton = document.querySelector('[data-js="prev-button"]');
-prevButton.addEventListener("prev", () => {
+prevButton.addEventListener("click", () => {
     console.log("Go to previous question");
 });
 
@@ -23,12 +23,12 @@ const secondChoiceButton = document.querySelector('[data-js="choice-button-2"]')
 firstChoiceButton.addEventListener("click", () => {
     firstChoiceButton.classList.add("active-choice");
     secondChoiceButton.classList.remove("active-choice");
-    console("Selected first choice");
+    console.log("Selected first choice");
 });
 
 // 4. When clicking on the second choice, the button should be shown with an active state. Do you see why it isn't working?
 secondChoiceButton.addEventListener("click", () => {
     firstChoiceButton.classList.remove("active-choice");
-    secondChoiceButton.classList.remove("active-choice");
+    secondChoiceButton.classList.add("active-choice");
     console.log("Selected second choice");
 });
