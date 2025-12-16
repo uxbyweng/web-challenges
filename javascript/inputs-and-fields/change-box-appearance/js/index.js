aada7aa6a4da0df28dd252335b4b061049f1c148
Karsten Weng <karsten@weng.eu>
2025-12-16 18:41:06 +0100

    challenge(result): change box appearance

--- javascript/inputs-and-fields/change-box-appearance/js/index.js
+++ javascript/inputs-and-fields/change-box-appearance/js/index.js
@@ -1,1 +1,22 @@
 console.clear();
+
+// BOX
+const box = document.querySelector('[data-js="box"]');
+
+// INPUTS
+const colorInput = document.querySelector('[data-js="input-color"]');
+const radiusInput = document.querySelector('[data-js="input-radius"]');
+const rotationInput = document.querySelector('[data-js="input-rotation"]');
+
+colorInput.addEventListener("input", () => {
+    box.style.backgroundColor = `hsl(${colorInput.value}, 80%, 50%)`;
+});
+
+radiusInput.addEventListener("input", () => {
+    box.style.borderRadius = `${radiusInput.value}%`;
+});
+
+rotationInput.addEventListener("input", () => {
+    console.log(rotationInput.value);
+    box.style.transform = `rotate(${rotationInput.value}deg)`;
+});
