263c303df9023fbc88f84259e50a5709e1fb1b64
Karsten Weng <karsten@weng.eu>
2025-12-16 16:16:02 +0100

    challenge(result): input tu uppercase

--- javascript/inputs-and-fields/input-to-uppercase/js/index.js
+++ javascript/inputs-and-fields/input-to-uppercase/js/index.js
@@ -1,1 +1,7 @@
 console.clear();
+const firstInput = document.querySelector('[data-js="first-input"]');
+const upperCaseButton = document.querySelector('[data-js="button-uppercase"]');
+
+upperCaseButton.addEventListener("click", () => {
+    firstInput.value = firstInput.value.toUpperCase();
+});
