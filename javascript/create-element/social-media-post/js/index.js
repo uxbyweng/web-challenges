c157d739cecdd6d1b110e6166429b6f4c272f695
Karsten Weng <karsten@weng.eu>
2025-12-18 18:19:09 +0100

    challenge(result): social media post

--- javascript/create-element/social-media-post/js/index.js
+++ javascript/create-element/social-media-post/js/index.js
@@ -1,12 +1,47 @@
 console.clear();
 
 function handleLikeButtonClick(event) {
-  const buttonElement = event.target;
-  buttonElement.classList.toggle("post__button--liked");
+    const buttonElement = event.target;
+    buttonElement.classList.toggle("post__button--liked");
 }
 
-const likeButton = document.querySelector('[data-js="like-button"]');
-likeButton.addEventListener("click", handleLikeButtonClick);
+// const likeButton = document.querySelector('[data-js="like-button"]');
+// likeButton.addEventListener("click", handleLikeButtonClick);
 
-// Exercise:
-// Use document.createElement() and append another social media post to the body.
+// create article
+const article = document.createElement("article");
+article.classList.add("post");
+document.body.append(article);
+
+// create paragraph
+const pText = document.querySelector('[class="post__content"]').innerHTML;
+const p = document.createElement("p");
+p.classList.add("post__content");
+p.textContent = pText;
+article.append(p);
+
+// create footer
+const footer = document.createElement("footer");
+footer.classList.add("post__footer");
+article.append(footer);
+
+// create @username
+const userName = document.querySelector('[class="post__username"]').innerHTML;
+const span = document.createElement("span");
+span.classList.add("post__username");
+span.textContent = userName;
+footer.append(span);
+
+// create button
+const btnText = document.querySelector('[data-js="like-button"]').innerHTML;
+const button = document.createElement("button");
+// button.addEventListener("click", handleLikeButtonClick);
+button.setAttribute("type", "button");
+button.classList.add("post__button");
+button.setAttribute("data-js", "like-button");
+button.textContent = btnText;
+footer.append(button);
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", handleLikeButtonClick));
+console.log(buttons);
