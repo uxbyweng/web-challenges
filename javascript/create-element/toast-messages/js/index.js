2ce5503c140258c5af58a59a2618f51916ac7d90
Karsten Weng <karsten@weng.eu>
2025-12-18 15:56:10 +0100

    challenge(result): toast messages

--- javascript/create-element/toast-messages/js/index.js
+++ javascript/create-element/toast-messages/js/index.js
@@ -5,9 +5,12 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+    const li = document.createElement("li");
+    li.innerHTML = `I'm a toast message.`;
+    li.classList.add("toast-container__message");
+    toastContainer.append(li);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+    toastContainer.innerHTML = "";
 });
